ee4cc54732d784e5268913452f45de35bc8ef240
nachi-green <149391126+nachi-green@users.noreply.github.com>
2023-12-20 22:50:57 +0900

    kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>課題007</title>
+    </head>
+
+    <body>
+        <p>
+            <?php
+            $Array = ['name' => 'origin', 'price' => 200, 'weight' => 160];
+            print_r($Array);
+            ?>
+        </p>
+    </body>
+</html>
